41887ca48fb34cfffb45c40bb35936fc41ae3164
naveenkumar1710 <113573484+naveenkumar1710@users.noreply.github.com>
2022-09-29 22:16:50 +0530

    Add files via upload

---
+++ calculator1.java
@@ -0,0 +1,11 @@
+public class calculator1{
+        
+       public static void main(String[]args){
+            calculator naveen=new calculator();
+            naveen.addition(5,5);
+            naveen.sub(5,3);
+            naveen.multiple(2,2);
+            naveen.division(10,2);
+            System.out.println(naveen.sub(3,2));
+}
+}
